e3a8e7d5ac8279f88cdbac728c127c47daa196fa
k-nrt <k-nrt@mail.com>
2019-07-03 23:32:53 +0900

    sprite_blink を修正
    グローバル変数の初期化方法が変更されていたので対応した。

--- projects/sprite_blink/crt0.s
+++ projects/sprite_blink/crt0.s
@@ -38,11 +38,14 @@ init:
 
 	;; Ordering of segments for the linker.
 	.area	_HOME
-	.area   _CODE
+	.area	_CODE
+	.area	_INITIALIZER
 	.area   _GSINIT
 	.area   _GSFINAL
 
-	.area   _DATA
+	.area	_DATA
+	.area	_INITIALIZED
+	.area	_BSEG
 	.area   _BSS
 	.area   _HEAP
 
@@ -51,6 +54,15 @@ _exit::
 	
 	.area   _GSINIT
 gsinit::
+	ld	bc, #l__INITIALIZER
+	ld	a, b
+	or	a, c
+	jr	Z, gsinit_next
+	ld	de, #s__INITIALIZED
+	ld	hl, #s__INITIALIZER
+	ldir
+
+gsinit_next:
 	.area   _GSFINAL
 	ret
-
+	
