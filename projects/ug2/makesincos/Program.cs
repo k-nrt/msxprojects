d2523494ee318bc1d8aea17c001cfc41fb2ff110
k-nrt <k-nrt@mail.com>
2020-05-19 00:52:02 +0900

    SinCos テーブル生成を変更
    高精度と低精度テーブルの生成を追加した。

--- projects/ug2/makesincos/Program.cs
+++ projects/ug2/makesincos/Program.cs
@@ -10,20 +10,39 @@ namespace makesincos
 	{
 		static void Main(string[] args)
 		{
-			int nCount = 64;
-
 			System.IO.StreamWriter writer = new System.IO.StreamWriter("sin_table.inc");
 
+            writer.WriteLine("// hp (16bit) sin pi/2 plus");
+            Write(writer, 0, 64, 256, 0xffff, 32768.0f,"(s16)0x{0,2:X4}");
+            writer.WriteLine("// hp (16bit) sin pi/2 minus");
+            Write(writer, 128, 64, 256, 0xffff, 32768.0f, "(s16)0x{0,2:X4}");
+
+            writer.WriteLine("// lp (8bit) sin 2pi");
+            Write(writer, 0, 256, 256, 0xff, 128.0f, "(s8)0x{0,2:X2}");
+
+            writer.Close();
+            System.Console.ReadLine();
+        }
+
+        static void Write(System.IO.StreamWriter writer, int nOffset, int nCount, int nResolution, int nMask, double fScale, string strFormat)
+        { 
 			for( int i = 0 ; i < nCount ; i++ )
 			{
-				double fRad = (((double)i)/((double)nCount))*Math.PI/2.0;
+				double fRad = Math.PI*2.0*((double)(i+nOffset))/(double)(nResolution);
 				double fSin = Math.Sin( fRad );
 				
-				fSin *= 256.0f;
-				int nSin = ((int) Math.Ceiling(fSin)) & 0xffff;
+				fSin *= fScale;
+				int nSin = 0;
+                if((i+nOffset) < (nResolution/2))
+                { 
+                    nSin = ((int) Math.Ceiling(fSin)) & nMask;
+                }
+                else
+                {
+                    nSin = ((int)Math.Floor(fSin)) & nMask;
+                }
 
-
-				writer.Write(String.Format("(s16)0x{0,2:X4}", nSin));
+                writer.Write(String.Format(strFormat, nSin));
 		
 				if( i%8 == 7 )
 				{
@@ -34,31 +53,6 @@ namespace makesincos
 					writer.Write(",");
 				}
 			}
-
-			for (int i = 0; i < nCount; i++)
-			{
-				double fRad = (((double)(i+128)) / ((double)nCount)) * Math.PI / 2.0;
-				double fSin = Math.Sin(fRad);
-
-				fSin *= 256.0f;
-				int nSin = ((int)Math.Floor(fSin)) & 0xffff;
-				
-
-				writer.Write(String.Format("(s16)0x{0,2:X4}", nSin));
-
-				if (i % 8 == 7)
-				{
-					writer.WriteLine(",");
-				}
-				else
-				{
-					writer.Write(",");
-				}
-			}
-
-
-			writer.Close();
-			System.Console.ReadLine();
-		}
+        }
 	}
 }
