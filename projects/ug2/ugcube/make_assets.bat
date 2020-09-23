2773e25a7222db6f82c22be5f242873678b64e1e
k-nrt <k-nrt@mail.com>
2020-09-23 22:28:58 +0900

    [ugcube] mtk 用モデル追加
    爆発とビームのモデルを追加。
    アセット生成も修正。

--- projects/ug2/ugcube/make_assets.bat
+++ projects/ug2/ugcube/make_assets.bat
@@ -3,10 +3,11 @@ setlocal enabledelayedexpansion
 
 set MakeModel=..\makemodel\bin\Release\makemodel.exe
 set AssetRoot=.\assets
-set Models=e1
 
-for %%i in (%Models%) do (
-    echo model %%i
-    "%MakeModel%" -o "%%i.inc" -i %AssetRoot%\%%i.mqo -name %%i -type ugxmesh
-    if %errorlevel% neq 0 exit /b
-)
+"%MakeModel%" -i %AssetRoot%\e1.mqo -prefix enemy1 -o mtk_mesh_enemy1.inc -name Enemy1  -type ugxmesh
+"%MakeModel%" -i %AssetRoot%\e1.mqo -prefix beam   -o mtk_mesh_beam.inc   -name Beam    -type ugxmesh
+
+"%MakeModel%" -i %AssetRoot%\explosion.mqo -prefix exp0 -o mtk_mesh_exp0.inc -name Exp0 -type ugxmesh
+"%MakeModel%" -i %AssetRoot%\explosion.mqo -prefix exp1 -o mtk_mesh_exp1.inc -name Exp1 -type ugxmesh
+"%MakeModel%" -i %AssetRoot%\explosion.mqo -prefix exp2 -o mtk_mesh_exp2.inc -name Exp2 -type ugxmesh
+"%MakeModel%" -i %AssetRoot%\explosion.mqo -prefix exp3 -o mtk_mesh_exp3.inc -name Exp3 -type ugxmesh
