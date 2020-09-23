da58103fbcb8db66a6fbda39c6cdefc1b18e82d4
k-nrt <k-nrt@mail.com>
2020-09-23 22:31:10 +0900

    [ugcube] MTK 修正
    自機の弾
    当たり判定
    爆発
    などを追加。

--- projects/ug2/ugcube/mtk_main.c
+++ projects/ug2/ugcube/mtk_main.c
@@ -5,8 +5,14 @@
 #include "clip.h"
 #include "flipper.h"
 #include "vdp_command.h"
+
 #include "mesh.h"
-#include "e1.inc"
+#include "mtk_mesh_beam.inc"
+#include "mtk_mesh_enemy1.inc"
+#include "mtk_mesh_exp0.inc"
+#include "mtk_mesh_exp1.inc"
+#include "mtk_mesh_exp2.inc"
+#include "mtk_mesh_exp3.inc"
 
 static u16 s_vertices[64];
 
@@ -68,7 +74,7 @@ void MtkMeshDrawNoClip(const SMtkMesh *pMesh)
 	PersDrawLines(pMesh->m_pIndices, pMesh->m_nbLines);
 }
 
-#define MTK_MODEL_LOD_MAX (3)
+#define MTK_MODEL_LOD_MAX (4)
 typedef struct tMtkModel
 {
 	SMtkMesh m_meshes[MTK_MODEL_LOD_MAX];
@@ -189,9 +195,9 @@ void MtkInputScan()
 	}
 
 	{
-		u8 trigger = msxBiosGetTrigger(2);
+		u8 trigger = msxBiosGetTrigger(3);
 		//. shift.
-		trigger |= msxBiosReadKeyMatrix(6) & 0x01;
+		trigger |= ~msxBiosReadKeyMatrix(6) & 0x01;
 		if (trigger)
 		{
 			down |= kMtkInput_Thrust;
@@ -210,6 +216,27 @@ void MtkInputScan()
 		g_mtkInput.m_up = up;
 	}
 }
+
+typedef struct tMtkEffect
+{
+	s16x3 m_position;
+	s16x3 m_velocity;
+	u8 m_type;
+	u8 m_timer;
+} SMtkEffect;
+
+enum EMtkEffectType
+{
+	kMtkEffectType_None,
+	kMtkEffectType_Explosion,
+};
+
+#define MTK_EFFECT_MAX (3)
+extern SMtkEffect g_mtkEfects[MTK_EFFECT_MAX];
+extern void MtkEffectInit();
+extern u8 MtkEffectSpawn(enum EMtkEffectType type, const s16x3 *pPosition, const s16x3 *pVelocity);
+extern void MtkEffectUpdate();
+
 typedef struct tMtkPlayer
 {
 	s16x3 m_velocity;
@@ -221,14 +248,96 @@ extern void MtkPlayerUpdate();
 
 typedef struct tMtkEnemy
 {
+	u8 m_status;
 	s16x3 m_position;
+	s16x3 m_velocity;
 } SMtkEnemy;
 #define MTK_ENEMY_MAX (3)
+
+enum EMtkEnemyStatus
+{
+	kMtkEnemyStatus_Idle,
+	kMtkEnemyStatus_Move,
+};
 
 extern SMtkEnemy g_mtkEnemies[MTK_ENEMY_MAX];
 
 extern void MtkEnemyInit();
 extern void MtkEnemyUpdate();
+
+enum EMtkShotStatus
+{
+	kMtkShotStatus_Idle,
+	kMtkShotStatus_Move,
+	kMtkShotStatus_Hit,
+};
+
+typedef struct tMtkShot
+{
+	u8    m_status;
+	s16x3 m_position;
+	s16x3 m_velocity;
+} SMtkShot;
+#define MTK_SHOT_MAX (4)
+extern SMtkShot g_mtkShots[MTK_SHOT_MAX];
+
+extern void MtkShotInit();
+extern void MtkShotUpdate();
+
+SMtkEffect g_mtkEfects[MTK_EFFECT_MAX];
+void MtkEffectInit()
+{
+	u8 i;
+	SMtkEffect *pEffect = g_mtkEfects;
+	for (i = 0; i < MTK_EFFECT_MAX; i++, pEffect++)
+	{
+		s16x3Set(pEffect->m_position, 0, 0, 0);
+		s16x3Set(pEffect->m_velocity, 0, 0, 0);
+		pEffect->m_type = kMtkEffectType_None;
+		pEffect->m_timer = 0;
+	}
+}
+
+u8 MtkEffectSpawn(enum EMtkEffectType type, const s16x3 *pPosition, const s16x3 *pVelocity)
+{
+	u8 i;
+	SMtkEffect *pEffect = g_mtkEfects;
+	UNUSED(type);
+	for (i = 0; i < MTK_EFFECT_MAX; i++, pEffect++)
+	{
+		if (pEffect->m_type != kMtkEffectType_None)
+		{
+			continue;
+		}
+		s16x3Copy(pEffect->m_position, (*pPosition));
+		s16x3Copy(pEffect->m_velocity, (*pVelocity));
+		pEffect->m_type = kMtkEffectType_Explosion;
+		pEffect->m_timer = 0;
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
+void MtkEffectUpdate()
+{
+	u8 i;
+	SMtkEffect *pEffect = g_mtkEfects;
+	for (i = 0; i < MTK_EFFECT_MAX; i++, pEffect++)
+	{
+		if (pEffect->m_type == kMtkEffectType_None)
+		{
+			continue;
+		}
+
+		s16x3Op(pEffect->m_position, pEffect->m_position, +, pEffect->m_velocity);
+		pEffect->m_timer++;
+		if (4 < pEffect->m_timer)
+		{
+			pEffect->m_type = kMtkEffectType_None;
+		}
+	}
+}
 
 SMtkPlayer g_mtkPlayer;
 void MtkPlayerInit()
@@ -240,7 +349,8 @@ void MtkPlayerUpdate()
 {
 	s16 vx = g_mtkPlayer.m_velocity.x;
 	s16 vy = g_mtkPlayer.m_velocity.y;
-
+	s16 vz = g_mtkPlayer.m_velocity.z;
+	
 	if (MtkInputIsDown(kMtkInput_Right))
 	{
 		if (vx < 8)
@@ -287,21 +397,48 @@ void MtkPlayerUpdate()
 		vy -= 1;
 	}
 
+	if (MtkInputIsDown(kMtkInput_Thrust))
+	{
+		if (vz < 8)
+		{
+			vz += 1;
+		}
+	}
+	else if (MtkInputIsDown(kMtkInput_Fire))
+	{
+		if (-8 < vz)
+		{
+			vz -= 1;
+		}
+	}
+	else if (vz < 0)
+	{
+		vz += 1;
+	}
+	else if (0 < vz)
+	{
+		vz -= 1;
+	}
+
 	g_mtkPlayer.m_velocity.x = vx;
 	g_mtkPlayer.m_velocity.y = vy;
+	g_mtkPlayer.m_velocity.z = vz;
 }
 
 SMtkEnemy g_mtkEnemies[MTK_ENEMY_MAX];
 void MtkEnemyInit()
 {
 	u8 i;
-	for (i = 0; i < MTK_ENEMY_MAX; i++)
+	SMtkEnemy *pEnemy = g_mtkEnemies;
+	for (i = 0; i < MTK_ENEMY_MAX; i++, pEnemy++)
 	{
 		s16 x = i;
 		s16 z = i;
 		x = -48 + x*48; 
 		z = 160 + 40*z;
-		s16x3Set(g_mtkEnemies[i].m_position,x,0,z);
+		s16x3Set(pEnemy->m_position, x, 0, z);
+		s16x3Set(pEnemy->m_velocity, 0, 0, 0);
+		pEnemy->m_status = kMtkEnemyStatus_Move;
 	}
 }
 
@@ -311,11 +448,72 @@ void MtkEnemyUpdate()
 	SMtkEnemy *pEnemy;
 	for (i = 0, pEnemy = g_mtkEnemies; i < MTK_ENEMY_MAX; i++, pEnemy++)
 	{
-		s16x3Op(pEnemy->m_position, pEnemy->m_position, +, g_mtkPlayer.m_velocity);
+		if (pEnemy->m_status == kMtkEnemyStatus_Move)
+		{
+			s16x3Op(pEnemy->m_position, pEnemy->m_position, -, g_mtkPlayer.m_velocity);
+		}
 	}
 }
 
+SMtkShot g_mtkShots[MTK_SHOT_MAX];
+void MtkShotInit()
+{
+	u8 i;
+	SMtkShot *pShot = g_mtkShots;
+	for(i=0; i < MTK_SHOT_MAX; i++, pShot++)
+	{
+		pShot->m_status = kMtkShotStatus_Idle;
+		s16x3Set(pShot->m_position, 0, 0, 0);
+		s16x3Set(pShot->m_velocity, 0, 0, 0);
+
+	}
+}
+
+void MtkShotUpdate()
+{
+	u8 i;
+	SMtkShot *pShot = g_mtkShots;
+	u8 spawn = MtkInputIsPush(kMtkInput_Fire) ? 1 : 0;
+	for(i=0; i < MTK_SHOT_MAX; i++, pShot++)
+	{
+		if (pShot->m_status == kMtkShotStatus_Move)
+		{
+			s16x3 velocity;
+			s16x3Op(velocity,pShot->m_velocity,-,g_mtkPlayer.m_velocity);
+			s16x3Op(pShot->m_position, pShot->m_position, +, velocity);
+
+			if (720 < pShot->m_position.z)
+			{
+				pShot->m_status = kMtkShotStatus_Idle;
+			}
+		}
+		else if (pShot->m_status == kMtkShotStatus_Hit)
+		{
+			s16x3 velocity;
+			s16x3Op(velocity,pShot->m_velocity,-,g_mtkPlayer.m_velocity);
+			s16x3Op(pShot->m_position, pShot->m_position, +, velocity);
+
+			pShot->m_status = kMtkShotStatus_Idle;
+		}
+		else if (spawn)
+		{
+			spawn = 0;
+			pShot->m_status = kMtkShotStatus_Move;
+			s16x3Set(pShot->m_position,  16, 8, 65);
+			pShot->m_velocity.x = g_mtkPlayer.m_velocity.x;
+			pShot->m_velocity.y = g_mtkPlayer.m_velocity.y;
+			pShot->m_velocity.z = 64;
+		}
+	}
+}
+
+
 SMtkModel g_modelEnemy;
+SMtkModel g_modelExp0;
+SMtkModel g_modelExp1;
+SMtkModel g_modelExp2;
+SMtkModel g_modelExp3;
+SMtkModel g_modelShot;
 
 void Mtk_Main(const char* pszTitle)
 {
@@ -333,15 +531,23 @@ void Mtk_Main(const char* pszTitle)
 	for (i = 0; i < 64; i++)
 	{
 		u8 rx = i << 2;
-		vertices[i] = PersRegisterVertices(g_meshe1.m_pVertices, g_meshe1.m_nbVertices, 0,rx,rz,1);
+		vertices[i] = PersRegisterVertices(g_meshEnemy1.m_pVertices, g_meshEnemy1.m_nbVertices, 0,rx,rz,1);
 	}
 	rz += 8;
 
 	MtkInputInit();
 	MtkPlayerInit();
+	MtkShotInit();
+	MtkEnemyInit();
 	MtkEnemyInit();
 
-	MtkModelCreate(&g_modelEnemy, &g_meshe1, 0, 128, 0);
+	MtkModelCreate(&g_modelEnemy, &g_meshEnemy1, 0, 128, 0);
+	MtkModelCreate(&g_modelShot, &g_meshBeam, 0, 0, 0);
+
+	MtkModelCreate(&g_modelExp0, &g_meshExp0, 0, 0, 0);
+	MtkModelCreate(&g_modelExp1, &g_meshExp1, 0, 0, 0);
+	MtkModelCreate(&g_modelExp2, &g_meshExp2, 0, 0, 0);
+	MtkModelCreate(&g_modelExp3, &g_meshExp3, 0, 0, 0);
 
 	for(;;)
 	{
@@ -349,21 +555,97 @@ void Mtk_Main(const char* pszTitle)
 
 		MtkInputScan();
 		MtkPlayerUpdate();
+		MtkShotUpdate();
 		MtkEnemyUpdate();
+
+		{
+			SMtkShot *pShot = g_mtkShots;
+			for (i = 0; i < MTK_SHOT_MAX; i++, pShot++)
+			{
+				if (pShot->m_status == kMtkShotStatus_Move)
+				{
+					u8 j;
+					SMtkEnemy *pEnemy = g_mtkEnemies;
+
+					for (j = 0; j < MTK_ENEMY_MAX; j++, pEnemy++)
+					{
+						if (pEnemy->m_status != kMtkEnemyStatus_Move)
+						{
+							continue;
+						}
+
+						if
+						(
+							pEnemy->m_position.x - 32 < pShot->m_position.x &&
+							pEnemy->m_position.y - 32 < pShot->m_position.y &&
+							pEnemy->m_position.z - 32 < pShot->m_position.z &&
+							pShot->m_position.x < pEnemy->m_position.x + 32 &&
+							pShot->m_position.y < pEnemy->m_position.y + 32 &&
+							pShot->m_position.z < pEnemy->m_position.z + 32
+						)
+						{
+							pEnemy->m_status = kMtkEnemyStatus_Idle;
+							pShot->m_status = kMtkShotStatus_Hit;
+							MtkEffectSpawn(kMtkEffectType_Explosion, &pEnemy->m_position, &pEnemy->m_velocity);
+							break;
+						}
+					}
+				}
+			}
+		}
+
+
+		MtkEffectUpdate();
 
         FlipperApplyForegroundColor();
 		VDPWait();
 
-		for(i=0;i<MTK_ENEMY_MAX;i++)
 		{
-			SMtkEnemy *pEnemy = &g_mtkEnemies[i];
-
-			MtkModelDrawNoClip(&g_modelEnemy, &pEnemy->m_position);	
+			SMtkEnemy *pEnemy = g_mtkEnemies;
+			for (i = 0; i < MTK_ENEMY_MAX; i++, pEnemy++)
+			{
+				if (pEnemy->m_status != kMtkEnemyStatus_Idle)
+				{
+					MtkModelDrawNoClip(&g_modelEnemy, &pEnemy->m_position);
+				}
+			}
 		}
 
-		//PersSetPosition(0,0,80);
-		//PersTransformNoClipVram(vertices[u8Frame], g_meshe1.m_nbVertices);
-		//PersDrawLines(g_meshe1.m_pIndices,g_meshe1.m_nbLines);
+		{
+			SMtkShot *pShot = g_mtkShots;
+			for (i = 0; i < MTK_SHOT_MAX; i++, pShot++)
+			{
+				if ( pShot->m_status != kMtkShotStatus_Idle)
+				{
+					MtkModelDrawNoClip(&g_modelShot, &pShot->m_position);
+				}
+			}
+		}
+
+		{
+			const SMtkModel *pModels[] = 
+			{
+				&g_modelExp0,
+				&g_modelExp1,
+				&g_modelExp2,
+				&g_modelExp3,
+				NULL, NULL, NULL, NULL
+			};
+			const SMtkEffect *pEffect = g_mtkEfects;
+
+			for (i = 0; i < MTK_EFFECT_MAX; i++, pEffect++)
+			{
+				if (pEffect->m_type == kMtkEffectType_None)
+				{
+					continue;
+				}
+				else
+				{
+					u8 index = pEffect->m_timer - 1;
+					MtkModelDrawNoClip(pModels[index], &pEffect->m_position);
+				}
+			}
+		}
 
 		u8Frame++;
 		if (64 <= u8Frame)
