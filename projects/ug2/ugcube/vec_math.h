114e8e793eab46a1cff63d491c79cf538e19602d
k-nrt <k-nrt@mail.com>
2024-08-31 14:12:13 +0900

    ugcube: fix operator priority
    演算子優先度の影響を受けないようにマクロ引数に括弧を追加しました。

--- projects/ug2/ugcube/vec_math.h
+++ projects/ug2/ugcube/vec_math.h
@@ -9,18 +9,18 @@ typedef struct
 	s16 x, y, z;
 } s16x3;
 
-#define s16x3Set(_v,_x,_y,_z) _v.x=_x;_v.y=_y;_v.z=_z
-#define s16x3Copy(_v,_r) _v.x=_r.x;_v.y=_r.y;_v.z=_r.z
-#define s16x3Op(_r,_a,_op,_b) _r.x=_a.x _op _b.x;_r.y=_a.y _op _b.y;_r.z=_a.z _op _b.z
+#define s16x3Set(_v,_x,_y,_z) (_v).x=(_x);(_v).y=(_y);(_v).z=(_z)
+#define s16x3Copy(_v,_r) (_v).x=(_r).x;(_v).y=(_r).y;(_v).z=(_r).z
+#define s16x3Op(_r,_a,_op,_b) (_r).x=(_a).x _op (_b).x;(_r).y=(_a).y _op (_b).y;(_r).z=(_a).z _op (_b).z
 
 typedef struct
 {
 	s8 x, y, z;
 } s8x3;
 
-#define s8x3Set(_v,_x,_y,_z) _v.x=_x;_v.y=_y;_v.z=_z
-#define s8x3Copy(_v,_r) _v.x=_r.x;_v.y=_r.y;_v.z=_r.z
-#define s8x3Op(_r,_a,_op,_b) _r.x=_a.x _op _b.x;_r.y=_a.y _op _b.y;_r.z=_a.z _op _b.z
+#define s8x3Set(_v,_x,_y,_z) (_v).x=(_x);(_v).y=(_y);(_v).z=(_z)
+#define s8x3Copy(_v,_r) _v.x=(_r).x;_v.y=(_r).y;_v.z=(_r).z
+#define s8x3Op(_r,_a,_op,_b) (_r).x=(_a).x _op (_b).x;(_r).y=(_a).y _op (_b).y;(_r).z=(_a).z _op (_b).z
 
 #define s8x3_RotateX(d,s,rot) {s8XY xy=SinCos_RotateXYS8Lp(s.y,s.z,rot); d.y=s8XY_GetX(xy); d.z=s8XY_GetY(xy);}
 #define s8x3_RotateY(d,s,rot) {s8XY xy=SinCos_RotateXYS8Lp(s.z,s.x,rot); d.z=s8XY_GetX(xy); d.x=s8XY_GetY(xy);}
